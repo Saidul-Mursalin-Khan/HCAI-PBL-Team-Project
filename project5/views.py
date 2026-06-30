dc31449391d6783185094e7643e99d72c8e5faa6
Asmaul-Urme <asmaul.urme@tuhh.de>
2026-06-30 15:10:04 +0200

    deletion of extra

--- project5/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    return render(request, 'project5/index.html', {
-        'title': 'Project 5: Reinforcement Learning with Human Feedback'
-    })
